af2cd60401b69f982e1ab7944b5f6067676cef6b
rnsabir72 <rehansabir72@gmail.com>
2026-06-06 21:17:40 +0500

    add heading in listing

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -3,9 +3,12 @@ import ContactListItem from './ContactListItem'
 
 const ContactList = ({data, handleGetUpdateContactData}) => {
     return (
+       <>
+       <h3>Contact Listing</h3>
         <ul>
             {data && data?.length ? data?.map((contact) => <ContactListItem key={contact.id} data={contact} handleGetUpdateContactData={handleGetUpdateContactData} />) : <h3>No Data Found!</h3>}
         </ul>
+       </>
     )
 }
 
