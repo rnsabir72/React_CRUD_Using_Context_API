aa0e6ca8e5826be46c36fffa578d377f032b7baa
rnsabir72 <rehansabir72@gmail.com>
2026-06-06 21:35:29 +0500

    Updated

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -3,12 +3,9 @@ import ContactListItem from './ContactListItem'
 
 const ContactList = ({data, handleGetUpdateContactData}) => {
     return (
-       <>
-       <h3>Contact Listing</h3>
         <ul>
             {data && data?.length ? data?.map((contact) => <ContactListItem key={contact.id} data={contact} handleGetUpdateContactData={handleGetUpdateContactData} />) : <h3>No Data Found!</h3>}
         </ul>
-       </>
     )
 }
 
